d60c595a0890d909e6f91e5768a09f485172fb13
Cristian Marín <cm961224@gmail.com>
2021-10-15 21:05:00 -0500

    Se modifica dockerfile y se agrega docker-compose para correr de forma local el servicio

--- Intecgra.Cerberus.Api/Filters/AppExceptionFilter.cs
+++ Intecgra.Cerberus.Api/Filters/AppExceptionFilter.cs
@@ -3,12 +3,21 @@ using System.Net;
 using Intecgra.Cerberus.Domain.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 
 namespace Intecgra.Cerberus.Api.Filters
 {
     [AttributeUsage(AttributeTargets.All)]
     public class AppExceptionFilter : ExceptionFilterAttribute
     {
+        private readonly ILogger<AppExceptionFilter> _logger;
+
+
+        public AppExceptionFilter(ILogger<AppExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public override void OnException(ExceptionContext context)
         {
             var msg = new
@@ -32,6 +41,7 @@ namespace Intecgra.Cerberus.Api.Filters
                         break;
                     default:
                     {
+                        _logger.Log(LogLevel.Error, context.Exception.Message);
                         context.HttpContext.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
                         context.Result = new ObjectResult(new
                             {message = "Ha ocurrido un error interno en la aplicación"});
